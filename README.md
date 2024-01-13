8dc3115f3ccd9188e3fac8e0d8ef7cb4d1164055
romaols <roar@k2.no>
2024-01-13 12:55:14 +0100

    Testversion

---
+++ README.md
@@ -0,0 +1,3 @@
+sauce4zwift Vikings TTT Panel
+========
+
---
+++ pages/src/mod.mjs
@@ -0,0 +1,9 @@
+import * as common from '/pages/src/common.mjs';
+
+async function main() {
+    console.log("Sauce Version:", await common.rpc.getVersion());
+}
+
+main();
+
+
